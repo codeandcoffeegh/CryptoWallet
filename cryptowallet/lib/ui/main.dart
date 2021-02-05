2eada34e1fdc7f7c512a2c33a55a0550dd294795
Abraham <atomety@gmail.com>
2021-02-05 00:47:56 +0000

    Authentication file
    Creating ui for authentication

---
+++ cryptowallet/lib/ui/main.dart
@@ -0,0 +1,20 @@
+import 'package:cryptowallet/ui/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  await Firebase.initializeApp();
+  runApp(CryptoWallet());
+}
+
+class CryptoWallet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoWallet',
+      home: Authentication(),
+    );
+  }
+}
+
+
